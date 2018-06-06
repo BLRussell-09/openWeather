20bb3b40fc0d329ac74004fd53baf03706492d76
Barry <blrussell09@gmail.com>
2018-06-06 13:25:07 -0500

    Added oauth for Google

--- javascripts/events.js
+++ javascripts/events.js
@@ -147,7 +147,29 @@ const authEvents = () =>
         // ...
       });
   });
-
+  // Log In With Google
+  $('#signInButtonGoogle').click(() =>
+  {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider).then((result) =>
+    {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+      // const token = result.credential.accessToken;
+      // The signed-in user info.
+      // const user = result.user;
+    // ...
+    }).catch((error) =>
+    {
+      // Handle Errors here.
+      $('#signin-error-msg').text(error.message);
+      console.error(error);
+      // The email of the user's account used.
+      // const email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      // const credential = error.credential;
+      // ...
+    });
+  });
   // Log Out Event
   $('#logoutNav').click((e) =>
   {
